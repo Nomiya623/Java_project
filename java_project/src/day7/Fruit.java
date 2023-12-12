766ae1a67c4b82cf87803690ef48deb64ce9a18c
tj-bu-706-17 <tj-bu-706-17@DESKTOP-U00SQ8C>
2023-12-12 15:47:41 +0900

    day7

---
+++ java_project/src/day7/Fruit.java
@@ -0,0 +1,45 @@
+package day7;
+
+public class Fruit {
+	private String name;
+	private String color;
+	private int price;
+
+	Fruit(String name, String color, int price){
+		this.name = name;
+		this.color = color;
+		this.price = price;
+	}
+		
+	//set, get 메소드
+	void setPrice(int price) {
+		if (price<=0) {
+			System.out.println("올바르지 않은 값입니다.");
+		} else {
+			this.price = price;
+		}
+		this.price = price;
+		}
+	//우클릭 source> Generate getters and setters> select all
+	//> after get/setName> generate
+	int getPrice() {
+		return price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+}//class

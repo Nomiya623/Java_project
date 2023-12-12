6072522368da6ccd9bc5b2626b407a2e97b679b9
tj-bu-706-17 <tj-bu-706-17@DESKTOP-U00SQ8C>
2023-12-12 11:57:37 +0900

    Sample ex

---
+++ java_project/src/day7/Ex2_Sample.java
@@ -0,0 +1,19 @@
+package day7;
+
+public class Ex2_Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			Sample ob1 = new Sample(3);
+			Sample ob2 = new Sample(4);
+			Sample s;
+			
+			s=ob2;
+			ob1 = ob2; //객체 지환
+			System.out.println(ob1.id);
+			System.out.println(ob2.id);
+		}
+		
+	}
+
---
+++ java_project/src/day7/Sample.java
@@ -0,0 +1,19 @@
+package day7;
+
+public class Sample {
+
+	int id;
+	public Sample(int x) {
+		this.id = x;
+		}
+	
+	public void set (int x) {
+		this.id = x;
+	}
+	
+	public int get() {
+		return this.id;
+	}
+	
+	
+}//class

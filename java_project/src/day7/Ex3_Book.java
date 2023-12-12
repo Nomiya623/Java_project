fb166425ace95ff9287b0fa07f1b90a136cf26dd
tj-bu-706-17 <tj-bu-706-17@DESKTOP-U00SQ8C>
2023-12-12 12:52:24 +0900

    Book example

---
+++ java_project/src/day7/Ex3_Book.java
@@ -0,0 +1,12 @@
+package day7;
+
+public class Ex3_Book {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book javaBook = new Book("Java JDK", "황기채", 3333);
+		Book holyBible = new Book("holyBible", 1);
+		Book emptyBook = new Book();
+	}//main
+
+}//class

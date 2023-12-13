f1827102239dbf812c9e046857e0062578412922
tj-bu-706-17 <tj-bu-706-17@DESKTOP-U00SQ8C>
2023-12-13 17:30:16 +0900

    day8_test

--- java_project/src/day8_test/FruitMain.java
+++ java_project/src/day8_test/FruitMain.java
@@ -1,24 +1,44 @@
 package day8_test;
 
+import java.util.Scanner;
+
 public class FruitMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 Scanner scanner = new Scanner(System.in);
+		 
 //		2. 메인클래스 생성 후 객체 메인 함수에서 2번에서 만든 클래스 객체 만들기
 //		 1) 클래스명 : FruitMain
 //		 2) Banana, Orange, Apple 객체 생성(값 임의로 넣기)
 
 		// Creating objects of Banana, Orange, and Apple
-
-		Banana banana = new Banana("바나나", "노란색", 1000);
-		Orange orange = new Orange("오렌지", "주황색", 1500);
-		Apple apple = new Apple("사과", "빨간색", 2000);
+		System.out.println("바나나 가격을 입력해주세요: ");
+		int bananaPrice = scanner.nextInt();
+		Banana banana = new Banana("바나나", "노란색", bananaPrice);
+		
+		System.out.println("오렌지 가격을 입력해주세요: ");
+		int orangePrice = scanner.nextInt();
+		Orange orange = new Orange("오렌지는", "노란색", orangePrice);
+		
+		System.out.println("사과 가격을 입력해주세요: ");
+		int applePrice = scanner.nextInt();
+		Apple apple = new Apple("사과", "빨간색", applePrice);
+		
+		
 
 		// Printing price information for each fruit
 		banana.printPrice();
 		orange.printPrice();
 		apple.printPrice();
-
+		
+		// total Sum
+		int totalSum  = banana.getPrice() + orange.getPrice() + apple.getPrice();
+		System.out.println("3개의 과일 가격 총합은: " + totalSum + "원입니다.");
+		
+		//close the Scanner
+		scanner.close();
+		
+		
 	}
 
 }

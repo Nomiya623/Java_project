679a7bdc84cd69543e20033852ac45bc559dd724
tj-bu-706-17 <tj-bu-706-17@DESKTOP-U00SQ8C>
2023-12-18 18:08:26 +0900

    day11_final

--- java_project/src/day9_test/ClassTest.java
+++ java_project/src/day9_test/ClassTest.java
@@ -1,311 +1,53 @@
-package day9_test;
-
-public class ClassTest {
-
-//	[4번]
-//	다음 2개의 static 가진 ArrayUtil 클래스를 만들어보자. 다음 코드의 실행 결과를 참고하여 concat()와 print()를 작성하여 ArrayUtil 클래스를 완성하라.
+//1. 다음 코드의 문제점을 설명하고, 올바른 방법으로 if문을 다시 작성 하시오.
+//String str1 = "자바";
+//String str2 = new String("자바");
+//if(str1 == str2){ // 문자열 비교
+//    // 문자열이 같다면 출력
+//};
 //
-//	class ArrayUtil {
-//	   public static int[] concat(int[] a, int[] b) {
-//	            /* 배열 a와 b를 연결한 새로운 배열 리턴 */
-//	   }
-//	   public static void print(int[] a) {
-//	      /* 배열 a 출력 */
-//	   }
-//	}
+//2. 반복문을 이용하여 구구단을 작성하시오.
+//단, 5단은 출력하지 말 것.
+//(for, while, do~while중 1가지 선택)
 //
-//	public class StaticEx {
-//
-//	   public static void main(String[] args) {
-//	      // TODO Auto-generated method stub
-//	      int[] array1 = {1, 5, 7, 9};
-//	      int[] array2 = {3, 6, -1, 100, 77};
-//	      int[] array3 = ArrayUtil.concat(array1, array2);
-//	      ArrayUtil.print(array3);
-//	   }
-//
-//	}
-//
-//	[ 1  5  7  9  3  6  -1  100  77 ]
-
-//	class ArrayUtil {
-//	    public static int[] concat(int[] a, int[] b) {
-//	        // Concatenate arrays a and b
-//	        int[] result = new int[a.length + b.length];
-//	        System.arraycopy(a, 0, result, 0, a.length);
-//	        System.arraycopy(b, 0, result, a.length, b.length);
-//	        return result;
-//	    }
-//
-//	    public static void print(int[] a) {
-//	        // Print the elements of array a
-//	        System.out.print("[ ");
-//	        for (int i = 0; i < a.length; i++) {
-//	            System.out.print(a[i]);
-//	            if (i < a.length - 1) {
-//	                System.out.print(" ");
-//	            }
-//	        }
-//	        System.out.println("]");
-//	    }
-//	}
-//
-//	public class StaticEx {
-//
-//	    public static void main(String[] args) {
-//	        int[] array1 = {1, 5, 7, 9};
-//	        int[] array2 = {3, 6, -1, 100, 77};
-//	        int[] array3 = ArrayUtil.concat(array1, array2);
-//	        ArrayUtil.print(array3);
-//	    }
-//	}
+//3. 아래와 같이 배열을 선언하고 가장 큰 값과 0번째 인덱스 값과 위치를 바꾸시오.
+//    // int arr[] = {3, 5, 2, 9, 8};
+//    // 결과 : {9, 5, 2, 3, 8}
 //	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-//	[5번]
-//	다음과 같은 Dictionary 클래스가 있다. 실행 결과와 같이 작동하도록 Dictionary 클래스의 kor2Eng() 메소드와 DicApp 클래스를 작성하라.
+//4. 랜덤 구구단 문제를 출력하고 정답을 입력받으시오.
+//단, 구구단은 5번 반복하며, 정답 입력 시 정답&오답 여부 및 점수를 출력해주어야 함.
+//출력 [ 9 * 7  = 63 ]
+//    [ 정답입니다. ]
+//    [ 3 * 2 = 10 ]
+//    [ 오답입니다. 정답은 6입니다.]
+//    [ 2 * 8 = 16 ]
+//    [ 정답입니다. ]
+//        ..............
+//    [ 총 80점입니다. ] // 한문제당 20점
 //
-//	class Dictionary {
-//	   private static String[] kor = {"사랑", "아기", "돈", "미래", "희망"};
-//	   private static String[] eng = {"love", "baby", "money", "future","hope"};
-//	   public static String kor2Eng(String word) { /*검색 코드 작성*/ }
-//	}
-//	한영 단어 검색 프로그램입니다.
-//	한글 단어?희망
-//	희망은 hope
-//	한글 단어?아가
-//	아가는 저의 사전에 없습니다.
-//	한글 단어?아기
-//	아기는 baby
-//	한글 단어?그만
-
-	// 텍스트 입력은 Scanner클래스 객체의 next()메소드를 이용하여 입력받을 수 있다.
-	
-	
-//	import java.util.Scanner;
+//5. * 숫자 1을 입력할 경우 '입력 부분', 2를 입력할 경우 '삭제 부분', 3을 입력할 경우 '출력 부분'
+//그 외의 값을 입력할 경우 '종료 부분'을 실행 한다.
 //
-//	class Dictionary {
-//	    private static String[] kor = {"사랑", "아기", "돈", "미래", "희망"};
-//	    private static String[] eng = {"love", "baby", "money", "future", "hope"};
+//1). 입력 부분
+//-> key를 String, value를 Object로 받는 HashMap 생성
+//-> 다음과 같이 map에 스캐너로 입력된 값 저장
+//-> key : name, value : 이름 (ex, 홍길동)
+//-> key : java, value : 자바 점수 (ex, 20), 단 0~30 사이 값이 아니면 다시 입력
+//-> key : oracle, value : 오라클 점수 (ex, 35), 단 0~50 사이 값이 아니면 다시 입력
+//-> key : html, value : html 점수(ex, 15), 단 0~20 사이 값이 아니면 다시 입력
 //
-//	    public static String kor2Eng(String word) {
-//	        for (int i = 0; i < kor.length; i++) {
-//	            if (kor[i].equals(word)) {
-//	                return eng[i];
-//	            }
-//	        }
-//	        return "저의 사전에 없습니다.";
-//	    }
-//	}
+//=> 해당 map을 ArrayList 객체에 저장
 //
-//	public class DicApp {
-//	    public static void main(String[] args) {
-//	        Scanner scanner = new Scanner(System.in);
-//	        System.out.println("한영 단어 검색 프로그램입니다.");
+//2). 삭제 부분
+//스캐너로 입력한 값과 map의 name value 값이 동일할 경우 리스트에서 삭제
 //
-//	        while (true) {
-//	            System.out.print("한글 단어?");
-//	            String input = scanner.next();
+//-> ex, 스캐너로 "홍길동"을 입력하면 name의 value가 "홍길동"인 map을 리스트에서 제거
 //
-//	            if (input.equals("그만")) {
-//	                System.out.println("프로그램을 종료합니다.");
-//	                break;
-//	            }
+//3). 출력 부분
+//-> 콘솔에 '이름 : 홍길동, 총점 : 70' 과 같은 형태로 출력
+//-> 총점은 java + oracle + html value의 총 합
 //
-//	            String result = Dictionary.kor2Eng(input);
-//	            System.out.println(input + "은(는) " + result);
-//	        }
-//
-//	        scanner.close();
-//	    }
-//	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-//상속형	
-	
-//	class ColorTV extends TV {
-//	    private int colors;
-//
-//	    public ColorTV(int size, int colors) {
-//	        super(size);
-//	        this.colors = colors;
-//	    }
-//
-//	    public void printProperty() {
-//	        System.out.println(getSize() + " inches " + colors + " colors");
-//	    }
-//	}
-//
-//	public class Main {
-//	    public static void main(String[] args) {
-//	        ColorTV myTV = new ColorTV(32, 1024);
-//	        myTV.printProperty();
-//	    }
-//	}
-//	
-	
-
-//	
-//	class IPTV extends ColorTV {
-//	    private String ipAddress;
-//
-//	    public IPTV(String ipAddress, int size, int colors) {
-//	        super(size, colors);
-//	        this.ipAddress = ipAddress;
-//	    }
-//
-//	    @Override
-//	    public void printProperty() {
-//	        System.out.println("My IPTV has " + ipAddress + " address, " + getSize() + " inches, " + colors + " colors");
-//	    }
-//	}
-//
-//	public class Main {
-//	    public static void main(String[] args) {
-//	        IPTV iptv = new IPTV("192.1.1.2", 32, 2048);
-//	        iptv.printProperty();
-//	    }
-//	}
-//	
-	
-	
-	
-	
-//	class ColorPoint extends Point {
-//	    private String color;
-//
-//	    public ColorPoint(int x, int y, String color) {
-//	        super(x, y);
-//	        this.color = color;
-//	    }
-//
-//	    public void setColor(String color) {
-//	        this.color = color;
-//	    }
-//
-//	    @Override
-//	    public String toString() {
-//	        return color + " color point at (" + getX() + "," + getY() + ")";
-//	    }
-//	}
-//
-//	public class Main {
-//	    public static void main(String[] args) {
-//	        ColorPoint cp = new ColorPoint(5, 5, "YELLOW");
-//	        cp.setXY(10, 20);
-//	        cp.setColor("RED");
-//	        String str = cp.toString();
-//	        System.out.println(str + " is.");
-//	    }
-//	}
-	
-	
-//	class ColorPoint extends Point {
-//	    private String color;
-//
-//	    public ColorPoint() {
-//	        super(0, 0);
-//	        this.color = "BLACK";
-//	    }
-//
-//	    public ColorPoint(int x, int y) {
-//	        super(x, y);
-//	        this.color = "BLACK";
-//	    }
-//
-//	    public void setColor(String color) {
-//	        this.color = color;
-//	    }
-//
-//	    @Override
-//	    public String toString() {
-//	        return color + " color point at (" + getX() + "," + getY() + ")";
-//	    }
-//	}
-//
-//	public class Main {
-//	    public static void main(String[] args) {
-//	        ColorPoint zeroPoint = new ColorPoint(); // (0,0) BLACK point
-//	        System.out.println(zeroPoint.toString() + " is.");
-//	        ColorPoint cp = new ColorPoint(10, 10); // (10,10) BLACK point
-//	        cp.setXY(5, 5);
-//	        cp.setColor("RED");
-//	        System.out.println(cp.toString() + " is.");
-//	    }
-//	}
-	
-	
-	
-//	
-//	class Point3D extends Point {
-//	    private int z;
-//
-//	    public Point3D(int x, int y, int z) {
-//	        super(x, y);
-//	        this.z = z;
-//	    }
-//
-//	    public void moveUp() {
-//	        z++;
-//	    }
-//
-//	    public void moveDown() {
-//	        z--;
-//	    }
-//
-//	    public void move(int x, int y) {
-//	        super.move(x, y);
-//	    }
-//
-//	    public void move(int x, int y, int z) {
-//	        super.move(x, y);
-//	        this.z = z;
-//	    }
-//
-//	    @Override
-//	    public String toString() {
-//	        return "(" + getX() + "," + getY() + "," + z + ") point";
-//	    }
-//	}
-//
-//	public class Main {
-//	    public static void main(String[] args) {
-//	        Point3D p = new Point3D(1, 2, 3);
-//	        System.out.println(p.toString() + " is.");
-//	        p.moveUp();
-//	        System.out.println(p.toString() + " is.");
-//	        p.moveDown();
-//	        p.move(10, 10);
-//	        System.out.println(p.toString() + " is.");
-//	        p.move(100, 200, 300);
-//	        System.out.println(p.toString() + " is.");
-//	    }
-//	}
-//	
-	
+//4). 종료 부분
+//-> 프로그램 종료
 	
 	
 	
@@ -358,5 +100,3 @@ public class ClassTest {
 	
 	
 	
-	
-}

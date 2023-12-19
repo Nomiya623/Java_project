ae6b9b0151cca5dfcd812bf41d347e677adeb29b
tj-bu-706-17 <tj-bu-706-17@DESKTOP-U00SQ8C>
2023-12-19 17:54:06 +0900

    Test Sample error fix

--- java_project/src/TestSample/MapTestEx.java
+++ java_project/src/TestSample/MapTestEx.java
@@ -1,5 +1,7 @@
 package TestSample;
+
 import java.util.*;
+
 //* 숫자 1을 입력할 경우 '입력 부분', 2를 입력할 경우 '삭제 부분', 3을 입력할 경우 '출력 부분'
 //그 외의 값을 입력할 경우 '종료 부분'을 실행 한다.
 public class MapTestEx {
@@ -7,76 +9,88 @@ public class MapTestEx {
 	public static int score(String sub, int max) {
 		Scanner s = new Scanner(System.in);
 		int type;
-		
-		while(true) {
+
+		while (true) {
 			type = s.nextInt();
-			if(type <0 || type> max) {
+			if (type < 0 || type > max) {
 				System.out.println(sub + "눈 0~" + max + "사이에 값");
 			} else {
 				break;
 			}
 		}
-	return type;
-	
-	
-	
-	
-	
-	
+		return type;
+
 	}
+
 	public static void main(String[] args) {
-	Scanner s = new Scanner(System.in);
-	ArrayList<HashMap<String, Object>> list = new ArrayList<>();
-	
+		Scanner s = new Scanner(System.in);
+		ArrayList<HashMap<String, Object>> list = new ArrayList<>();
+
 //	2). 삭제 부분	
-	
-	while(true) {
-		System.out.println("== (1)입력부분  (2) 삭제부분 (3) 종료부분 ==");
-		System.out.println("숫자를 입력해주세요: ");
-		 int input = s.nextInt();
-		 if (input == 1) {
-			 HashMap<String, Object>map = new HashMap<>();
-			 System.out.println("입력 부분: ");
-			 map.put("name", s.next());
-			 
-			 System.out.println("자바 점수를 입력해주세요: ");
-			 map.put("java", s.next());
-			 map.put("java", score("java", 30));
-			
-			 System.out.println("오라클 점수를 입력해주세요: ");
-			 map.put("oracle", s.next());
-			 map.put("oracle", score("oracle", 50));
-			 System.out.println("html 점수를 입력해주세요: ");
-			 map.put("html", s.next());
-			 map.put("oracle", score("oracle", 20));
-			 
-			 list.add(map);
-			 System.out.println("저장되었습니다.");
+
+		while (true) {
+			System.out.println("== (1)입력 (2) 삭제 (3) 종료 ==");
+			System.out.println("입력 부분: ");
+			int input = s.nextInt();
+			if (input == 1) {
+				HashMap<String, Object> map = new HashMap<>();
+				
+				System.out.println("이름을 입력해주세요: ");
+				map.put("name", s.next());
+//				System.out.println("자바 점수를 입력해주세요: ");
+//				map.put("java", s.next());
+				map.put("java", score("java", 30));
+//				System.out.println("오라클 점수를 입력해주세요: ");
+//				map.put("oracle", s.next());
+				map.put("oracle", score("oracle", 50));
+//				System.out.println("html 점수를 입력해주세요: ");
+//				map.put("html", s.next());
+				map.put("html", score("html", 20));
+
+				list.add(map);
+				System.out.println("저장되었습니다.");
 //			 3). 출력 부분			 
-		 } else if(input == 2) {
-			 System.out.println("삭제 부분: ");
-		 } else if(input == 3) {
-			 System.out.println("출력 부분: ");
-		 } else {
-			 System.out.println("종료되었습니다.");
-		 }
-		
-		 boolean flg = false;
-		if (!flg) {
+			} else if (input == 2) {
+				System.out.println("삭제 부분: ");
+				String keyword = s.next();
+				for (int i = 0; i < list.size(); i++) {
+				String name = (String) list.get(i).get("name");
+				if(keyword.equals(name)) {
+					list.remove(i);
+					break;
+					}
+				}
+			} else if (input == 3) {
+				System.out.println("출력 부분: ");
+				String keyword = s.next();
+				for (int i = 0; i < list.size(); i++) {
+					String name = (String) list.get(i).get("name");
+					if(keyword.equals(name)) {
+						int sum = list.get(i).get(i) + list.get(i).get("oracle") + list.get(i).get("html");
+						System.out.println("이름: " + name + ", 총점" + sum);
+						break;
+			} else {
+				System.out.println("종료되었습니다.");
+			}
+
+			boolean flg = false;
+			if (!flg) {
 				System.out.println("찾는 사람이 없습니다.");
+				
 			} else if (input == 3) {
 				System.out.println("종료되었습니다.");
 				break;
 			} else {
 				System.out.println("1, 2, 3 중에 하나를 입력해주세요.");
 			}
+			
 		}
 	}
-	
+
 	public static void result() {
 		Scanner s = new Scanner(System.in);
 		ArrayList<HashMap<String, Object>> list = new ArrayList<>();
-		
+
 		String name;
 		int totalScore;
 		int java;
@@ -84,28 +98,9 @@ public class MapTestEx {
 		int html;
 //		총합 계산
 //		System.out.println("이름: " + name + ", 총점" + totalScore);
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	}
 
+}
 
 	
-	
-	
-	
-	
-	
-}

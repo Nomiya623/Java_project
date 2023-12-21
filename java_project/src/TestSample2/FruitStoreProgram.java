86cc9402ce6f272cf6fa0272021341ed624ce3cc
Aliya <Aliya@Aliya>
2023-12-21 22:11:56 +0900

    repeat

--- java_project/src/TestSample2/FruitStoreProgram.java
+++ java_project/src/TestSample2/FruitStoreProgram.java
@@ -15,16 +15,18 @@ public class FruitStoreProgram {
 		// => (0개 이하의 내려가지 않도록)
 		// 3) 과일명 입력 받기
 		// => 입력받은 과일의 수 확인
-		
-		//직접 해보기
-		
-		//4 번 메뉴 
-		//과일의 가격변경
-		//과일이름 입력 받은 후 해당 과일이 있는 먼저 확인
-		// 과일이 잇을 경우 새로운 가격을 입력받아서 
-		//3번메뉴에서 개수확린할대 가격까지 같이 확인
+
+		// 직접 해보기
+
+		// 4 번 메뉴
+		// 과일의 가격변경
+		// 과일이름 입력 받은 후 해당 과일이 있는 먼저 확인
+		// 과일이 잇을 경우 새로운 가격을 입력받아서
+		// 3번메뉴에서 개수확린할대 가격까지 같이 확인
 		Scanner s = new Scanner(System.in);
 		ArrayList<HashMap<String, Object>> list = new ArrayList<>();
+		ArrayList<String>list= new ArrayList<>;
+		HashMap<String, Object>map = new HashMap<>();
 		System.out.println("과일 주문 시스템");
 		while (true) {
 			System.out.println("(1)과일추가 (2)판매 (3)확인 (4)가격 변경 (5)(그외) 종료");
@@ -35,7 +37,6 @@ public class FruitStoreProgram {
 				System.out.print("과일명 입력 : ");
 				String name = s.next();
 				map.put("name", name);
-
 
 				// 2.중복된 과일명을 입력 시 (ex, 오랜지가 있는데 1번 메뉴에서 오렌지 다시 입력 시
 				// 이때는 가겨을 입력 받지 말고
@@ -69,10 +70,10 @@ public class FruitStoreProgram {
 				}
 
 			} else if (select == 2) {
-				//판매
+				// 판매
 				System.out.print("구매할 과일 입력해주세요! : ");
 				String name = s.next();
-				
+
 				System.out.print("구매할 개수를 입력해주세요! : ");
 				int cnt = s.nextInt();
 
@@ -88,39 +89,38 @@ public class FruitStoreProgram {
 							System.out.println(name + "의 남은 갯수 : " + (fruitCnt - cnt) + "개");
 						} else {
 							System.out.println("재고 부족.");
-							System.out.println(name + "의 현재 남은 개수는 " + fruitCnt + "개 입니다." );
+							System.out.println(name + "의 현재 남은 개수는 " + fruitCnt + "개 입니다.");
 						}
 					}
 				}
 			} else if (select == 3) {
 				System.out.print("확인할 과일 입력해주세요! : ");
 				String name = s.next();
-				
+
 				for (int i = 0; i < list.size(); i++) {
 					String fruitName = (String) list.get(i).get("name");
 					if (fruitName.equals(name)) {
-						HashMap<String, Object>map = new HashMap<>();
+						HashMap<String, Object> map = new HashMap<>();
 						map = list.get(i);
-						int fruitCnt = (int)map.get("cnt");
+						int fruitCnt = (int) map.get("cnt");
 						System.out.println(name + "의 현재 남은 개수는 : " + fruitCnt + "개");
 						System.out.println(name + "의 현재 남은 개수는 " + list.get(i).get("name"));
 					}
 				}
-			 } else if (select == 4) {
-	                System.out.print("가격을 변경할 과일 입력해주세요! : ");
-	                String name = s.next();
+			} else if (select == 4) {
+				System.out.print("가격을 변경할 과일 입력해주세요! : ");
+				String name = s.next();
 
-	                for (int i = 0; i < list.size(); i++) {
-	                    String fruitName = (String) list.get(i).get("name");
-	                    if (fruitName.equals(name)) {
-	                        System.out.print("새로운 가격 입력 : ");
-	                        int newPrice = s.nextInt();
-	                        list.get(i).put("price", newPrice);
-	                        System.out.println(name + "의 가격이 변경되었습니다!");
-	                    }
-	                }	
-				
-				
+				for (int i = 0; i < list.size(); i++) {
+					String fruitName = (String) list.get(i).get("name");
+					if (fruitName.equals(name)) {
+						System.out.print("새로운 가격 입력 : ");
+						int newPrice = s.nextInt();
+						list.get(i).put("price", newPrice);
+						System.out.println(name + "의 가격이 변경되었습니다!");
+					}
+				}
+
 			} else {
 				System.out.println("종료되었습니다!");
 				break;
@@ -133,5 +133,5 @@ public class FruitStoreProgram {
 			System.out.println("1, 2, 3, 4 또는 종류하려면 5 메뉴 중에 하나를 선택해주세요. ");
 		}
 
-	}//main
+	}// main
 }// class
